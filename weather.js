732567d87737cb89b49b388600ef62ce6a2e1df6
Rokas Grigaravicius <grigroka@gmail.com>
2018-11-28 10:31:32 +0200

    getWeather method

---
+++ weather.js
@@ -0,0 +1,19 @@
+'use strict';
+class Weather {
+  constructor(city, countryCode) {
+    this.apiKey = 'f1beb0fc3b79ceb4360cb9cf9abe3f05';
+    this.city = city;
+    this.countryCode = countryCode;
+  }
+
+  // Fetch weather from API
+  async getWeather() {
+    const response = await fetch(
+      `api.openweathermap.org/data/2.5/weather?q=${this.city},${
+        this.countryCode
+      }&APPID=${this.apiKey}`
+    );
+    const responseData = await response.json();
+    return responseData;
+  }
+}
